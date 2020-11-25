8af37adcc0d174d8c644c17011b063063db4581e
June <zkitun@163.com>
2020-11-25 21:39:54 +0800

    添加注释

--- src/core/instance/inject.js
+++ src/core/instance/inject.js
@@ -7,10 +7,7 @@ import { defineReactive, toggleObserving } from '../observer/index';
 export function initProvide (vm: Component) {
 	const provide = vm.$options.provide;
 	if (provide) {
-		vm._provided =
-
-				typeof provide === 'function' ? provide.call(vm) :
-				provide;
+		vm._provided = typeof provide === 'function' ? provide.call(vm) : provide;
 	}
 }
 
